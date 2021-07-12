33c11f2ab6be981513ee35f9b11c32ed136634c8
alessandro-Porcu-85 <34552726+alessandro-Porcu-85@users.noreply.github.com>
2021-07-12 10:35:58 +0200

    Added files

---
+++ Bootstrap_exercise/E-COMMERCE/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+
+
+
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            560: {
+                items: 2,
+                nav: false
+            },
+            960: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+
+    // click to scroll top
+    $('.move-up span').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+});
